131678eaf1037888764af3747d4df56970cf1c81
Shashidhar <anumallashashidhar6gmail.com>
2023-09-05 19:16:03 +0530

    first commit

---
+++ src/main/java/primera/App.java
@@ -0,0 +1,13 @@
+package primera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
